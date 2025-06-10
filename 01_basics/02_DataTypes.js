0bc619805f1f1a822d30bbac5d9b9688ca56d035
__Arijit Das__ <132340245+ArijitDas2005@users.noreply.github.com>
2025-06-10 08:50:09 +0000

    datatypes

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -3,3 +3,24 @@
 // alert( 3 + 3 ) // we are using nodejs, not browser
 
 console.log(3 + 3)
+
+let name = "Arijit"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Arijit");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
