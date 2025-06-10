7aed59040de3bb7ab6e5dde5b7ab8b19feeefae8
__Arijit Das__ <132340245+ArijitDas2005@users.noreply.github.com>
2025-06-10 09:41:53 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,30 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "something.com"
+
+let anothername = myYoutubename
+anothername = "new.youtube.com"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne = {
+    email : "userOne@gmail.com",
+    upi : "userOne@upi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "arijit@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
